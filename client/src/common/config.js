b0935f35f654b2a9fec83706c3508c15d04e9508
zeeshanRN <frontenddev59@gmail.com>
2024-05-11 15:32:28 +0500

    change_Base_Url

--- client/src/common/config.js
+++ client/src/common/config.js
@@ -1,6 +1,7 @@
 const Config = {
   // BASE_URL: "http://localhost:4000",
-  BASE_URL: "https://api.darkpixels-tax.shop",
+  // BASE_URL: "https://api.darkpixels-tax.shop",
+  BASE_URL: "http://api.facts-na.com",
 }
 
 export default Config
