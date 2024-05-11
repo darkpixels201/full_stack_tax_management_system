5bd6d0ba6c03dcf8fbe4ce78371dec4dbbbcd078
zeeshanRN <frontenddev59@gmail.com>
2024-05-11 16:31:16 +0500

    add_ssl_certificate

--- client/src/common/config.js
+++ client/src/common/config.js
@@ -1,7 +1,7 @@
 const Config = {
   // BASE_URL: "http://localhost:4000",
   // BASE_URL: "https://api.darkpixels-tax.shop",
-  BASE_URL: "http://api.facts-na.com",
+  BASE_URL: "https://api.facts-na.com",
 }
 
 export default Config
