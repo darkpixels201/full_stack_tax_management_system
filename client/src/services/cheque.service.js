780569d94aa3ac6f2febed62993a749bd7f8cc96
darkpixels201 <darkpixels201@gmail.com>
2024-08-12 15:14:00 +0500

    Cheque_issue_resolve

--- client/src/services/cheque.service.js
+++ client/src/services/cheque.service.js
@@ -8,8 +8,12 @@ export const chequeList = async () => {
   return await axios.get(`cheque/all-cheques`);
 };
 
-export const chequeByBankName = async (payload) => {
-  return await axios.get(`cheque/by-bank/${payload}`)
+export const chequeByBankName = async (bankName, selectedUserId) => {
+  console.log(" Selected Payload",selectedUserId)
+  const payload = {bankName, selectedUserId}
+  console.log("Tow Values From Payload",payload)
+  return await axios.post(`cheque/by-bank`, payload)
+  // return await axios.get(`cheque/by-bank/${bank}`, selectedUserId)
 }
 
 export const deleteCheque = async (id) => {
