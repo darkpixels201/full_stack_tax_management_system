5890a08055bea74af28bf01bc4a2f3b547d7ae27
darkpixels201 <darkpixels201@gmail.com>
2025-02-26 12:38:48 +0500

    Change_the_Backend_api_Link

--- client/src/common/config.js
+++ client/src/common/config.js
@@ -1,10 +1,10 @@
 const Config = {
-  BASE_URL: "http://localhost:4000",
+  // BASE_URL: "http://localhost:4000",
   // BASE_URL: "https://api.darkpixels-tax.shop",
   // BASE_URL: "https://api.facts-na.com",
   // BASE_URL: "https://api.demo-facts.pixelboxpos.com",
-  // BASE_URL: "https://zaidfactsapi-facts.pixelboxpos.com",
-  
+  // BASE_URL: "https://zaidfactsapi.pixelboxpos.com",
+   BASE_URL: "https://zaidfactsapi.zaidpos.shop", 
 }
 
 export default Config
